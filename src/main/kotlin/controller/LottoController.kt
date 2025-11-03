6b538a1be379580dacb77ba70965291eec405006
Jo Eungyeong <joeungyeong23@gmail.com>
2025-11-03 10:07:37 +0900

    feat(lotto): 로또 수량 및 번호 출력 기능 추가

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -1,12 +1,17 @@
 package controller
 
 import domain.Amount
+import domain.Lottos
+import domain.RandomLottoNumberGenerator
+import domain.dto.LottosDto
 import view.InputView
 import view.OutputView
 
 class LottoController {
     fun run() {
         val amount = getPurchaseAmount()
+        val lottos = generateLottos(amount)
+        OutputView.displayLottoCountAndNumbers(lottos.toDto())
     }
 }
 
@@ -15,6 +20,16 @@ private fun getPurchaseAmount(): Amount {
         try {
             OutputView.displayPurchaseAmountPrompt()
             return Amount.from(InputView.readInput())
+        } catch (e: IllegalArgumentException) {
+            println(e.message.toString())
+        }
+    }
+}
+
+private fun generateLottos(amount: Amount): Lottos {
+    while (true) {
+        try {
+            return Lottos.generate(amount, RandomLottoNumberGenerator())
         } catch (e: IllegalArgumentException) {
             println(e.message.toString())
         }
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,9 +1,28 @@
 package view
 
+import domain.dto.LottoDto
+import domain.dto.LottosDto
+
 object OutputView {
     private const val PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요."
+    private const val LOTTO_COUNT_MESSAGE_SUFFIX = "개를 구매했습니다."
 
     fun displayPurchaseAmountPrompt() {
         println(PURCHASE_AMOUNT_PROMPT)
+    }
+
+    fun displayLottoCountAndNumbers(lottos: LottosDto) {
+        printLottoCount(lottos.count)
+        printLottoNumbers(lottos.lottos)
+    }
+
+    private fun printLottoCount(count: Int) {
+        println("\n${count}$LOTTO_COUNT_MESSAGE_SUFFIX")
+    }
+
+    private fun printLottoNumbers(lottos: List<LottoDto>) {
+        lottos.forEach {
+            println(it.numbers)
+        }
     }
 }
