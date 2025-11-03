a6def75a6bc199c86d8e9931ef9f62fc070b26cd
Jo Eungyeong <joeungyeong23@gmail.com>
2025-11-03 14:32:03 +0900

    refactor(validator): 줄바꿈 삭제

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -3,7 +3,6 @@ package util
 import exception.ErrorMessage
 
 object Validator {
-
     fun validateNotBlank(input: String) {
         require(input.isNotBlank()) { ErrorMessage.INPUT_BLANK }
     }
