dc31aea1225aeee9111227cbe69d54006530eeae
Jo Eungyeong <joeungyeong23@gmail.com>
2025-11-03 16:17:53 +0900

    fix(rank): 오류 방지를 위해 ELSE 추가

--- src/main/kotlin/domain/Rank.kt
+++ src/main/kotlin/domain/Rank.kt
@@ -2,12 +2,13 @@ package domain
 
 import java.math.BigDecimal
 
-enum class Rank(val matchCount: Int, val isBonusNumberMatch: Boolean = false, val prize: Int) {
+enum class Rank(val matchCount: Int = 0, val isBonusNumberMatch: Boolean = false, val prize: Int = 0) {
     THREE_MATCH(matchCount = 3, prize = 5_000),
     FOUR_MATCH(matchCount = 4, prize = 50_000),
     FIVE_MATCH(matchCount = 5, isBonusNumberMatch = false, prize = 1_500_000),
     FIVE_MATCH_AND_BONUS_MATCH(matchCount = 5, isBonusNumberMatch = true, prize = 30_000_000),
-    SIX_MATCH(matchCount = 6, prize = 2_000_000_000);
+    SIX_MATCH(matchCount = 6, prize = 2_000_000_000),
+    ELSE;
 
     fun culculatePrize(count: Int): BigDecimal {
         return prize.toBigDecimal().multiply(count.toBigDecimal())
@@ -16,9 +17,11 @@ enum class Rank(val matchCount: Int, val isBonusNumberMatch: Boolean = false, va
     companion object {
         fun from(matchCount: Int, isBonusNumberMatch: Boolean): Rank {
             if (matchCount == 5) {
-                return entries.first() { it.matchCount == matchCount && it.isBonusNumberMatch == isBonusNumberMatch }
+                return entries.firstOrNull {
+                    it.matchCount == matchCount && it.isBonusNumberMatch == isBonusNumberMatch
+                } ?: ELSE
             }
-            return entries.first() { it.matchCount == matchCount }
+            return entries.firstOrNull() { it.matchCount == matchCount } ?: ELSE
         }
     }
 }
