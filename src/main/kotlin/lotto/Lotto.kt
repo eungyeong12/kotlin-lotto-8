b7dbceabf7d0c2c5acf4ee0fb5538ec57833966a
Jo Eungyeong <joeungyeong23@gmail.com>
2026-01-07 06:18:36 +0900

    init

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,27 +1,8 @@
 package lotto
 
-import constant.Constants.LOTTO_COUNT
-import constant.Constants.MAX_NUMBER
-import constant.Constants.MIN_NUMBER
-import domain.BonusNumber
-import domain.Rank
-import domain.WinningNumbers
-import domain.dto.LottoDto
-import exception.ErrorMessage
-
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == LOTTO_COUNT) { ErrorMessage.LOTTO_NUMBER_COUNT_INVALID }
-        require(numbers.all { it in MIN_NUMBER..MAX_NUMBER }) { ErrorMessage.LOTTO_NUMBERS_RANGE_INVALID }
-        require(numbers.size == numbers.distinct().size) { ErrorMessage.LOTTO_NUMBERS_DUPLICATE }
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    fun getRank(winningNumbers: WinningNumbers, bonusNumber: BonusNumber): Rank {
-        return Rank.from(
-            winningNumbers.getMatchCount(numbers),
-            bonusNumber.isBonusNumberMatch(numbers)
-        )
-    }
-
-    fun toDto(): LottoDto = LottoDto(numbers.sorted())
 }
